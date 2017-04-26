dcabcffb0344731d72d6517f22e9b7062a4b0252
idk <problemsolver@openmailbox.org>
2017-04-25 22:49:58 -0400

    fixed tv playlists

--- envsetup.sh
+++ envsetup.sh
@@ -14,6 +14,30 @@ echo "song: download a song using a youtube url,
 alias song=musicplaylist
 echo "musicpl: download a song playlist using a youtube url,
         song \$youtube-url"
+alias radrama=audiodrama
+echo "radrama: download a radio drama using a youtube url,
+        radrama \$youtube-url"
+alias radramapl=audiodramaplaylist
+echo "radramapl: download a radio drama playlist using a youtube url,
+        radramapl \$youtube-url"
+alias news=audionews
+echo "news: download a news broadcast using a youtube url,
+        news \$youtube-url"
+alias newspl=audionewspl
+echo "newspl: download a news broadcast playlist using a youtube url,
+        newspl \$youtube-url"
+alias book=audiobook
+echo "book: download an audiobook using a youtube url,
+        book \$youtube-url"
+alias bookpl=audiobookplaylist
+echo "bookpl: download a song playlist using a youtube url,
+        bookpl \$youtube-url"
+alias standup=audiocomedy
+echo "standup: download a song using a youtube url,
+        standup \$youtube-url"
+alias standuppl=audiocomedyplaylist
+echo "standuppl: download a song playlist using a youtube url,
+        standuppl \$youtube-url"
 alias film=movie
 echo "film: download a movie using a youtube url,
         film \$youtube-url"
